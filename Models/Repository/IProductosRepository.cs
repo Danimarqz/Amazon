09de643b44602c749933012a665d0080ac080e00
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2024-05-30 20:08:15 +0200

    ProductosRepository initialized

---
+++ Models/Repository/IProductosRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Amazon.Models.Repository
+{
+    public interface IProductosRepository
+    {
+        public Task<IEnumerable<Productos>> GetAll();
+        public Task<Productos> GetById(int id);
+        public void Create(Productos productos);
+        public void Update(Productos productos);
+        public void Delete(Productos productos);
+
+    }
+}
---
+++ Models/Repository/ProductosRepository.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Amazon.Models.Repository
+{
+    public class ProductosRepository : IProductosRepository
+    {
+        private readonly Conexion _conexion;
+
+        public ProductosRepository(Conexion conexion)
+        {
+            _conexion = conexion;
+        }
+        public void Create(Productos productos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Productos productos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Productos>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Productos> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Productos productos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
