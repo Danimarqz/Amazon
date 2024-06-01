0ba86936cffc0a89ca099d5125ac84aba37af8df
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-06-01 19:26:49 +0200

    Use Task so methods can be awaited

--- Models/Repository/ICarritoRepository.cs
+++ Models/Repository/ICarritoRepository.cs
@@ -3,12 +3,14 @@
     public interface ICarritoRepository
     {
         public Task<Carrito> GetCart(int userID);
-        public int GetCartID(int userID);
-        public Task<DetallesCarrito> GetAllCart(int userID);
-        public void Add(Carrito cart);
-        public void AddDetalles(DetallesCarrito detallesCarrito);
-        public void EditCarrito(Carrito cart);
-        public void EditDetallesCarrito(DetallesCarrito detallesCarrito);
-        protected Task<int> CalcularPrecioTotal(int carritoID);
+        public Task<IEnumerable<Carrito>> GetCartAdmin();
+        public Task<int> GetCartID(int userID);
+        public Task<IEnumerable<DetallesCarrito>> GetAllCart(int userID);
+        public Task Add(int userID);
+        public Task UpdateCart(int cartID);
+        public Task AddProducto(int productoID, int userID);
+        public Task EditCarrito(Carrito cart);
+        public Task EditDetallesProductoCarrito(DetallesCarrito detallesCarrito);
+        public Task DeleteProducto(int productID, int usuarioID);
     }
 }
