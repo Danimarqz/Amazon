2f624f96a7266860a81a2829a1d102cc5fd3c4ab
Daniel Márquez <113545666+Danimarqz@users.noreply.github.com>
2024-06-02 18:00:53 +0000

    can make global user null

--- Global.cs
+++ Global.cs
@@ -4,7 +4,7 @@ namespace Amazon
 {
     public class Global
     {
-        public static Usuarios user {  get; set; }
-        public static bool IsAdmin { get { return user.userType == "administrador"; } }
+        public static Usuarios? user {  get; set; }
+        public static bool IsAdmin { get { return user?.userType == "administrador"; } }
     }
 }
