07cc887f7a288c3492f90831fdd970dfa996c4ec
Mykyta Ryasny <MykytaRyasny@gmail.com>
2024-06-02 18:24:53 +0200

    small fix due to docker issues

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
+using System.Runtime.InteropServices;
 using Amazon.Models;
 using Amazon.Models.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
-
+Console.WriteLine(System.Environment.OSVersion.ToString());
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("Amazon")));
+
 //When using docker
-//builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("AmazonDocker")));
+if (System.Environment.OSVersion.ToString().Contains("Unix"))
+{
+    builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("AmazonDocker")));
+}
+else
+{
+    builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("Amazon")));
+}
 builder.Services.AddScoped<IUsuariosRepository, UsuariosRepository>();
 builder.Services.AddScoped<ICarritoRepository, CarritoRepository>();
 builder.Services.AddScoped<IProductosRepository, ProductosRepository>();
 builder.Services.AddScoped<ICarritoRepository, CarritoRepository>();
 builder.Services.AddScoped<IVentaRepository, VentaRepository>();
 builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options=>
+builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(15);
     options.Cookie.HttpOnly = true;
